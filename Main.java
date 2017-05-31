1d9292c9b5c22a0aef339fda9f60ff0744d7fb74
Théophile Walter <TheophileWalter@users.noreply.github.com>
2017-05-31 15:18:05 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,14 +5,10 @@ public class Main {
 
 	public static void main(String args[]) {
 		
-		// Send the name
-		
-		Scanner scan = new Scanner(System.in);
-		String line = scan.nextLine();
-		
-		if (line.equals("NAME?")) {
-			System.out.println("JFPDIX");
-		}
+		Scanner scan;
+		String line;
+		Plateau p;
+		Direction[] m;
 		
 		// CHeck for the challenge
 		if (args.length < 2) {
@@ -21,18 +17,55 @@ public class Main {
 		switch (args[1]) {
 		
 		case "epreuve1":
+
 			
-			Plateau p = new Plateau(scan);
+			// Send the name
+			scan = new Scanner(System.in);
+			line = scan.nextLine();
+			scan.nextLine();
 			
-			Direction[] m = p.mouvementsPossibles(0);
+			if (line.equals("NAME?")) {
+				System.out.println("JFPDIX");
+			}
 			
+			p = new Plateau(scan);
+			m = p.mouvementsPossibles(0);
 			System.out.println(m[(int)(Math.random()*m.length)]);
+
+			scan.close();
 			
 			break;
 			
-		}
+		case "epreuve2":
+			
+			// Send the name
+			scan = new Scanner(System.in);
+			line = scan.nextLine();
+			scan.nextLine();
+			
+			if (line.equals("NAME?")) {
+				System.out.println("JFPDIX");
+			}
 
-		scan.close();
+			do {
+				p = new Plateau(scan);
+				System.out.println(p.getPathToBonus(0));
+			} while (p.getNbTour() > 0);
+
+			scan.close();
+			
+			break;
+			
+		case "battle":
+			
+			// TODO
+			
+			break;
+			
+		default:
+			System.err.println("Error: Incorrect parameter!\nIt should be:\n	- epreuve1\n	- epreuve2\n	- epreuve3\n	- epreuve4\n	- battle");
+			
+		}
 		
 	}
 	
