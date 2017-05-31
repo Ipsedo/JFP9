6155279f124ad1d276a0ba4f019dcc6cc760b351
Ipsedo <berriens@gmail.com>
2017-05-31 11:59:49 +0200

    Modifié direction

--- Direction.java
+++ Direction.java
@@ -3,11 +3,11 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-
 public enum Direction {
 	NORTH(0, -1), SOUTH(0, 1), EAST(1, 0), WEST(-1, 0);
 
-	private static final List<Direction> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
+	private static final List<Direction> VALUES = Collections
+			.unmodifiableList(Arrays.asList(values()));
 	private final int dI, dJ;
 
 	private Direction(int di, int dj) {
@@ -25,5 +25,9 @@ public enum Direction {
 
 	public static Direction random() {
 		return VALUES.get(new Random().nextInt(VALUES.size()));
+	}
+
+	public static Direction[] getAllDirection() {
+		return new Direction[] { NORTH, SOUTH, EAST, WEST };
 	}
 }
