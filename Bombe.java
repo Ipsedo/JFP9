abb70908763a1b752787af845169ea4cfd7b7f15
Dexter <antoinelanco@gmail.com>
2017-05-31 14:01:01 +0200

    Add files via upload

---
+++ Bombe.java
@@ -0,0 +1,14 @@
+public class Bombe {
+	
+	private int player;
+	private int power;
+	private int nbTour;
+	
+	public Bombe(int player, int power, int nbTour){
+		this.player=player;
+		this.power=power;
+		this.nbTour=nbTour;
+	}
+	
+	
+}
