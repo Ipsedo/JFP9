913aa42d89acc0d0f8379316d330f3f6bc2a5c2c
Ipsedo <berriens@gmail.com>
2017-05-31 10:59:03 +0200

    Add cell class

---
+++ Cell.java
@@ -0,0 +1,20 @@
+
+public class Cell {
+ public static char INDESTRUCTIBLE_BLOC = 'X';
+ public static char DESTRUCTIBLE_BLOC = 'O';
+ public static char DESTRUCTIBLE_BLOC_POWER_BONUS = 'Y';
+ public static char DESTRUCTIBLE_BLOC_PRODUC_BONUS = 'Z';
+ public static char FREE_CELL = 'G';
+ public static char POWER_BONUS = 'H';
+ public static char PRODUC_BONUS = 'I';
+ 
+ private char cellState;
+ 
+ public Cell(char cellState) {
+	 this.cellState = cellState;
+ }
+ 
+ public void changeState(char newState) {
+	 this.cellState = newState;
+ }
+}
