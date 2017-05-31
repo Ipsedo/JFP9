94d9d5e402f8b892e065a10c1e4664ef788bb078
Théophile Walter <TheophileWalter@users.noreply.github.com>
2017-05-31 15:18:29 +0200

    Add files via upload

--- Direction.java
+++ Direction.java
@@ -2,32 +2,32 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-
+ 
 public enum Direction {
-	UP(-1, 0), DOWN(1, 0), RIGHT(0, 1), LEFT(0, -1);
-
-	private static final List<Direction> VALUES = Collections
-			.unmodifiableList(Arrays.asList(values()));
-	private final int dI, dJ;
-
-	private Direction(int di, int dj) {
-		this.dI = di;
-		this.dJ = dj;
-	}
-
-	public int dI() {
-		return dI;
-	}
-
-	public int dJ() {
-		return dJ;
-	}
-
-	public static Direction random() {
-		return VALUES.get(new Random().nextInt(VALUES.size()));
-	}
-
-	public static Direction[] getAllDirection() {
-		return new Direction[] { UP, DOWN, RIGHT, LEFT };
-	}
+    UP(-1, 0), DOWN(1, 0), RIGHT(0, 1), LEFT(0, -1), NOP(0, 0);
+ 
+    private static final List<Direction> VALUES = Collections
+            .unmodifiableList(Arrays.asList(values()));
+    private final int dI, dJ;
+ 
+    private Direction(int di, int dj) {
+        this.dI = di;
+        this.dJ = dj;
+    }
+ 
+    public int dI() {
+        return dI;
+    }
+ 
+    public int dJ() {
+        return dJ;
+    }
+ 
+    public static Direction random() {
+        return VALUES.get(new Random().nextInt(VALUES.size()));
+    }
+ 
+    public static Direction[] getAllDirection() {
+        return new Direction[] { UP, DOWN, RIGHT, LEFT };
+    }
 }
