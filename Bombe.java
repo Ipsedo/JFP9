41f8f40ff28625ad55c292145da578759d6fb9e2
Dexter <antoinelanco@gmail.com>
2017-05-31 14:13:15 +0200

    Add files via upload
    isindanger

--- Bombe.java
+++ Bombe.java
@@ -3,12 +3,27 @@ public class Bombe {
 	private int player;
 	private int power;
 	private int nbTour;
+	private int i;
+	private int j;
 	
-	public Bombe(int player, int power, int nbTour){
+	public Bombe(int player, int power, int nbTour, int i, int j){
+		this.i=i;
+		this.j=j;
 		this.player=player;
 		this.power=power;
 		this.nbTour=nbTour;
 	}
 	
-	
+	public boolean isInDanger(Player player){
+		if(this.i == player.getI()){
+			if(this.j+this.power>= player.getJ() && this.j-this.power<=player.getJ()){
+				return true;
+			}
+		}else if(this.j == player.getJ()){
+			if(this.i+this.power>= player.getJ() && this.i-this.power<=player.getJ()){
+				return true;
+			}
+		}
+		return false;
+	}
 }
