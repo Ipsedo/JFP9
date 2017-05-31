11f3a63c7ef468cb425fc3c39e50d76af1044b85
Dexter <antoinelanco@gmail.com>
2017-05-31 11:34:37 +0200

    inti plateau

--- Plateau.java
+++ Plateau.java
@@ -20,6 +20,33 @@ public class Plateau {
 		this.lineSize = lineSize;
 		this.columnSize = columnSize;
 	}
+	
+	public void inti(Scanner scan){
+		
+		String line = scan.nextLine();
+		System.out.println(line);
+		String[] lineCut = line.split(" ");
+		this.nbPlayer = lineCut[0];
+		this.nbTour = lineCut[1];
+		this.joueures = new Player[this.nbPlayer];
+		for(int i=0;i<this.nbPlayer;i++){
+			line = scan.nextLine();
+			lineCut = line.split(" ");
+			this.joueures[i] = new Player(lineCut[0],lineCut[1],lineCut[2],lineCut[3],lineCut[4],lineCut[5],lineCut[6]);
+		}
+		line = scan.nextLine();
+		lineCut = line.split(" ");
+		this.L = lineCut[0];
+		this.C = lineCut[1];
+		this.plateau = new Plateau[this.L][this.C];
+		for(int i=0;i<this.L; i++){
+			line = scan.nextLine();
+			lineCut = line.split("|");
+			for(int j = 0;j<this.C; j++){
+				this.plateau[i][j] = new Cell(lineCut[j+1]);
+			}
+		}
+	}
 
 	public void move(int joueur, Plateau.Move move) {
 		if (this.isCorrectMove(joueur, move)) {
